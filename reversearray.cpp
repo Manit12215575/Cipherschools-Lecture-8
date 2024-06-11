e7b96a14b4d2135dd8c9f832c004d8a86f49b5d2
Manit12215575 <119652083+Manit12215575@users.noreply.github.com>
2024-06-11 12:22:57 +0530

    Add files via upload

---
+++ reversearray.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a[10];
+cout<<"Give 10 intgers as input";
+for(int i=0;i<10;i++)
+{
+	cin>>a[i];
+}
+cout<<"the 10 given inputs are:"<<endl;
+for(int i=9;i>=0;i--)
+{
+	cout<<a[i]<<" ";
+}
+}
